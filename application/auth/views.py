b28afd79d2b0505e8171d553150190f4695b4d52
samilait <sami.p.laitinen@gmail.com>
2019-05-31 21:41:35 +0300

    Uloskirjautuminen ja userstories päivitys

--- application/auth/views.py
+++ application/auth/views.py
@@ -1,5 +1,5 @@
 from flask import render_template, request, redirect, url_for
-from flask_login import login_user
+from flask_login import login_user, logout_user
 
 from application import app
 from application.auth.models import User
@@ -22,3 +22,9 @@ def auth_login():
     login_user(user)
     # print("User " + user.name + " recognised")
     return redirect(url_for("index"))    
+
+
+@app.route("/auth/logout")
+def auth_logout():
+    logout_user()
+    return redirect(url_for("index"))
--- dokumentaatio/userstories.md
+++ dokumentaatio/userstories.md
@@ -1,5 +1,6 @@
 # Käyttötapaukset
 
+- Käyttäjä kirjautuu sovellukseen käyttäjätunnuksella ja salasanalla
 - Käyttäjä voi lisätä pelaajia tietokantaan. Pelaajan tiedot ovat: Nimi (Etunimi Sukunimi), Pelaajanumero ja Pelipaikka (KH, VL, OL, VP, OP, MV).
 - Käyttäjä voi listata kannassa olevat pelaajat.
 - Käyttäjä voi vaihtaa pelaajan pelipaikkaa listauksen yhteydessä.
