d8aa0d93b746d5983cca5ec8e28f12bf52077129
samilait <sami.p.laitinen@gmail.com>
2019-06-07 18:02:06 +0300

    heroku tietokanta vaihtoehto lisätty

--- application/__init__.py
+++ application/__init__.py
@@ -3,8 +3,13 @@ app = Flask(__name__)
 
 from flask_sqlalchemy import SQLAlchemy
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///icehockey.db"
-app.config["SQLALCHEMY_ECHO"] = True
+import os
+
+if os.environ.get("HEROKU"):
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
+else:
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///icehockey.db"
+    app.config["SQLALCHEMY_ECHO"] = True
 
 db = SQLAlchemy(app)
 
@@ -40,4 +45,7 @@ def load_user(user_id):
     return User.query.get(user_id)
 
 
-db.create_all()
+try:
+    db.create_all()
+except:
+    pass
