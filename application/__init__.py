ccaed323e15de06cc2010b7e60042f9445b80eb8
samilait <sami.p.laitinen@gmail.com>
2019-06-22 13:53:45 +0300

    DB fixing

--- application/__init__.py
+++ application/__init__.py
@@ -15,45 +15,45 @@ db = SQLAlchemy(app)
 
 
 # kirjautuminen
-# from os import urandom
-# app.config["SECRET_KEY"] = urandom(32)
+from os import urandom
+app.config["SECRET_KEY"] = urandom(32)
 
-# from flask_login import LoginManager, current_user
-# login_manager = LoginManager()
-# login_manager.init_app(app)
+from flask_login import LoginManager, current_user
+login_manager = LoginManager()
+login_manager.init_app(app)
 
-# login_manager.login_view = "auth_login"
-# login_manager.login_message = "Please login to use this functionality."
+login_manager.login_view = "auth_login"
+login_manager.login_message = "Please login to use this functionality."
 
-# roles in login_required
-# from functools import wraps
+roles in login_required
+from functools import wraps
 
-# def login_required(role="ANY"):
-#     def wrapper(fn):
-#         @wraps(fn)
-#         def decorated_view(*args, **kwargs):
-#             if not current_user:
-#                 return login_manager.unauthorized()
+def login_required(role="ANY"):
+    def wrapper(fn):
+        @wraps(fn)
+        def decorated_view(*args, **kwargs):
+            if not current_user:
+                return login_manager.unauthorized()
 
-#             if not current_user.is_authenticated:
-#                 return login_manager.unauthorized()
+            if not current_user.is_authenticated:
+                return login_manager.unauthorized()
             
-#             unauthorized = False
+            unauthorized = False
 
-#             if role != "ANY":
-#                 unauthorized = True
+            if role != "ANY":
+                unauthorized = True
                 
-#                 for user_role in current_user.roles():
-#                     if user_role == role:
-#                         unauthorized = False
-#                         break
+                for user_role in current_user.roles():
+                    if user_role == role:
+                        unauthorized = False
+                        break
 
-#             if unauthorized:
-#                 return login_manager.unauthorized()
+            if unauthorized:
+                return login_manager.unauthorized()
             
-#             return fn(*args, **kwargs)
-#         return decorated_view
-#     return wrapper
+            return fn(*args, **kwargs)
+        return decorated_view
+    return wrapper
 
 # load application content
 from application import views
@@ -71,35 +71,34 @@ from application.matches import views
 # from application.goals import views
 
 # login
-from application.auth.models import User
-from os import urandom
-app.config["SECRET_KEY"] = urandom(32)
-
-from flask_login import LoginManager
-login_manager = LoginManager()
-login_manager.init_app(app)
-
-login_manager.login_view = "auth_login"
-login_manager.login_message = "Please login to use this functionality."
-
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(user_id)
-
-# from application.auth import models
-# from application.auth import views
-
-
-# login functionality, part 2
 # from application.auth.models import User
+# from os import urandom
+# app.config["SECRET_KEY"] = urandom(32)
 
+# from flask_login import LoginManager
+# login_manager = LoginManager()
+# login_manager.init_app(app)
+
+# login_manager.login_view = "auth_login"
+# login_manager.login_message = "Please login to use this functionality."
 
 # @login_manager.user_loader
 # def load_user(user_id):
 #     return User.query.get(user_id)
 
+from application.auth import models
+from application.auth import views
+
+
+# login functionality, part 2
+from application.auth.models import User
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+
 # database creation
-# db.create_all()
 try:
     db.create_all()
 except:
