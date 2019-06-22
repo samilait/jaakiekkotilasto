ba0e6d15176abfdccf186d947934eecaefa36b9c
samilait <sami.p.laitinen@gmail.com>
2019-06-22 14:00:17 +0300

    DB fixing

--- application/__init__.py
+++ application/__init__.py
@@ -14,18 +14,20 @@ else:
 db = SQLAlchemy(app)
 
 
-# kirjautuminen
+# login functionality
 from os import urandom
 app.config["SECRET_KEY"] = urandom(32)
 
 from flask_login import LoginManager, current_user
 login_manager = LoginManager()
-login_manager.init_app(app)
+login_manager.setup_app(app)
 
 login_manager.login_view = "auth_login"
 login_manager.login_message = "Please login to use this functionality."
 
-roles in login_required
+
+# roles in login_required
+
 from functools import wraps
 
 def login_required(role="ANY"):
